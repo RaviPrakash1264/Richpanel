3e5d6a36c3cfbb01e482f3f167b49a560e77e5c2
Ravi Prakash <ravimehra1264@gmail.com>
2021-08-20 01:25:15 +0530

    server.js file updated

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+const PORT = process.env.PORT || 5006 ;
 const express =require('express');
 const app = express();
 app.use(express.static(__dirname + '/public'));
 
-app.listen(5006,()=>{
-    console.log('Listening to the server 5005');
+app.listen(PORT,()=>{
+    console.log('Listening');
 })
